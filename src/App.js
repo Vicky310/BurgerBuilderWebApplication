5f104c02848c7df3de3f7fc765bdfc648db463a5
Vigneshwaran <vigneshwaran0310@gmail.com>
2020-03-29 19:08:58 -0400

    Commiting all the files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './hoc/Layout/Layout';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'; 
+import Logout from './containers/Auth/Logout/Logout';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+  render() {
+    let routes;
+    routes  = (
+      <Switch>
+        <Route path="/auth" exact component={asyncAuth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+
+    );
+    if(this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+        <Route path="/auth" exact component={asyncAuth} />
+        <Route path="/checkout" component={asyncCheckout} />
+        <Route path="/orders" exact component={asyncOrders} />
+        <Route path="/logout" exact component={Logout} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+        </Switch>
+      );
+    }
+
+    return (
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())  
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,33 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+    it('Store token on login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'sometoken',
+            userId: 'someid'
+        })).toEqual({
+            token: 'sometoken',
+            userId: 'someid',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+});
